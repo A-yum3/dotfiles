a3ae67c7e7d12411164905e7105d0464370556fe
A-yum3 <noikari73@gmail.com>
2021-10-23 15:37:54 +0900

    fix: bug

--- zsh/function.zsh
+++ zsh/function.zsh
@@ -1,11 +1,11 @@
 function gitmain() {
   git config --global user.name A-yum3
   git config --global user.email noikari73@gmail.com
-  # source ~/.zshrc
+  source ~/.zshrc
 }
 
 function gitsmv() {
   git config --global user.name ueyama-smv
   git config --global user.email y.ueyama@sonicmoov.com
-  # source ~/.zshrc
+  source ~/.zshrc
 }
