142b729283709b3a70bc56ea0fec8e5c3181931b
A-yum3 <noikari73@gmail.com>
2022-01-03 11:09:52 +0900

    complete settings for starship

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -29,7 +29,6 @@ zinit light mollifier/anyframe
 
 # 補完
 # zinit ice wait'0'; zinit light zsh-users/zsh-completions
-# autoload -Uz compinit && compinit
 zinit light marlonrichert/zsh-autocomplete
 
 zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
