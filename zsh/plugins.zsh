27d0cb336ce85f904926967904ec677c558062ec
A-yum3 <noikari73@gmail.com>
2022-01-03 12:18:45 +0900

    fix zinit installer

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,17 +1,12 @@
 
-# Plugin history-search-multi-word loaded with investigating.
-zinit load zdharma-continuum/history-search-multi-word
-
-# Two regular plugins loaded without investigating.
-# zinit light zsh-users/zsh-autosuggestions
-zinit light zdharma-continuum/fast-syntax-highlighting
+zinit for \
+    light-mode  zsh-users/zsh-autosuggestions \
+    light-mode  zdharma-continuum/fast-syntax-highlighting \
+                zdharma-continuum/history-search-multi-word
 
 # jq をインタラクティブに使える。JSONを標準出力に出すコマンドを入力した状態で `Alt+j` すると jq のクエリが書ける。
 # 要 jq
 zinit light reegnz/jq-zsh-plugin
-
-# Gitの変更状態がわかる ls。ls の代わりにコマンド `k` を実行するだけ。
-# zinit light supercrabtree/k
 
 # AWS CLI v2の補完。
 # 要 AWS CLI v2
@@ -23,9 +18,6 @@ zinit light drgr33n/oh-my-zsh_aws2-plugin
 
 # anyframeのセットアップ
 zinit light mollifier/anyframe
-
-# クローンしたGit作業ディレクトリで、コマンド `git open` を実行するとブラウザでGitHubが開く
-# zinit light paulirish/git-open
 
 # 補完
 # zinit ice wait'0'; zinit light zsh-users/zsh-completions
